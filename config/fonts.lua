b0a53d52b4f06f8ab2caf2bde4e49aa1b2c39b3b
wslDavid <dav1dch@outlook.com>
2024-05-26 16:58:02 +0800

    font

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,11 +2,12 @@ local wezterm = require("wezterm")
 local platform = require("utils.platform")
 
 local font = "Cascadia Mono NF"
-local font_size = platform().is_mac and 16 or 15
+local font_size = platform().is_mac and 16 or 12
+local font_family = platform().is_mac and { weight = "Light", italic = false } or { italic = false }
 -- local font_size = 16
 
 return {
-  font = wezterm.font(font, { weight = "Light", italic = false }),
+  font = wezterm.font(font, font_family),
   font_size = font_size,
 
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
