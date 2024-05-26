2bfb6dd24951d15e55480ae4e29e4e17654aab74
wslDavid <dav1dch@outlook.com>
2024-05-26 16:22:54 +0800

    font size

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 local platform = require("utils.platform")
 
 local font = "Cascadia Mono NF"
--- local font_size = platform().is_mac and 12 or 9
-local font_size = 16
+local font_size = platform().is_mac and 16 or 15
+-- local font_size = 16
 
 return {
   font = wezterm.font(font, { weight = "Light", italic = false }),
